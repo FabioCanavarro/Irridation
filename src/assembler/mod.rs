d890b0cd5ed5f10afb9a20cea58d0db3f8f1460f
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-01-11 22:50:27 +0700

    Made the Assembler struct, new() and default, and also added SymbolTable and AssemblerPhase

--- src/assembler/mod.rs
+++ src/assembler/mod.rs
@@ -1,3 +1,5 @@
+use std::default;
+
 use crate::instruction::Opcode;
 pub mod opcode_parser;
 pub mod operand_parser;
@@ -15,6 +17,50 @@ pub enum Token{
     LabelDeclaration{name: String},
     LabelUsage{name: String},
     Directive{name: String}
+}
+
+
+#[derive(Debug, PartialEq, Clone, Default)]
+pub enum AssemblerPhase {
+    #[default]
+    First,
+    Second
+}
+
+
+#[derive(Debug)]
+pub enum SymbolTable{
+    First
+}
+
+impl SymbolTable{
+    pub fn new() -> SymbolTable{
+        SymbolTable::First
+    }
+}
+
+
+impl Default for SymbolTable{
+    fn default() -> Self {Self::new()}
+}
+
+#[derive(Debug)]
+pub struct Assembler{
+    pub phase: AssemblerPhase,
+    pub symbols: SymbolTable
+}
+
+impl Assembler{
+    pub fn new () -> Assembler{
+        Assembler{
+            phase: AssemblerPhase::First,
+            symbols: SymbolTable::new()
+        }
+    }
+}
+
+impl Default for Assembler{
+    fn default() -> Self {Self::new()}
 }
 
 
