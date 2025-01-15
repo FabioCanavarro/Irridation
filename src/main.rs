4badb63b5a9e0eaf3d9ea8a1c94da91a3f662b08
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-01-15 21:04:25 +0700

    Ran rustfmt

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ extern crate nom;
 #[macro_use]
 extern crate clap;
 
-use std::{fs::File, io::Read, path::Path};
 use assembler::Assembler;
-use clap::{App,Arg,SubCommand};
+use clap::{App, Arg, SubCommand};
+use std::{fs::File, io::Read, path::Path};
 use vm::Vm;
 
 pub mod assembler;
@@ -14,12 +14,12 @@ pub mod instruction;
 pub mod repl;
 pub mod vm;
 
-fn start_repl(){
+fn start_repl() {
     let mut user_repl = repl::Repl::new();
     user_repl.run();
 }
 
-fn read_file(tmp: &str) -> String{
+fn read_file(tmp: &str) -> String {
     let filename = Path::new(tmp);
     let fl = File::open(filename);
     match fl {
@@ -28,40 +28,35 @@ fn read_file(tmp: &str) -> String{
             match f.read_to_string(&mut file_content) {
                 Ok(_) => file_content,
                 Err(e) => {
-                    println!("{:?}",e);
+                    println!("{:?}", e);
                     std::process::exit(1);
                 }
             }
-            
-        },
+        }
         Err(_) => {
             println!("Failed to read file");
             std::process::exit(1);
-
         }
     }
 }
-
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
     let target_file = matches.value_of("INPUT_FILE");
-    match target_file{
-        Some(fl) =>{
+    match target_file {
+        Some(fl) => {
             let program = read_file(fl);
             let mut vm = Vm::new();
             let mut asm = Assembler::new();
             let program = asm.assemble(&program);
 
-            if let Some(p) = program{
+            if let Some(p) = program {
                 vm.add_bytes(p);
                 vm.run();
                 std::process::exit(0);
             }
- 
-        },
+        }
         None => start_repl(),
-        
-   }
+    }
 }
