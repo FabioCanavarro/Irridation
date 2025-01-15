88183e1ad465656deb1cb9ae5f156b6f7bd23f00
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-01-15 20:34:35 +0700

    Added clap and made a new function for starting the repl

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 #[macro_use]
 extern crate nom;
+extern crate clap;
+use clap::{App,Arg,SubCommand};
 pub mod assembler;
 pub mod instruction;
 pub mod repl;
 pub mod vm;
-fn main() {
-    println!("Hello, world!");
+
+fn start_repl(){
     let mut user_repl = repl::Repl::new();
     user_repl.run();
+}
+
+fn main() {
+    start_repl();
 }
