9fc0e9c911560d4e6afa09250deccee919c0d675
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-01-15 20:46:49 +0700

    Function to read file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate nom;
 extern crate clap;
+use std::{fs::File, io::Read, path::Path};
+
 use clap::{App,Arg,SubCommand};
 pub mod assembler;
 pub mod instruction;
@@ -11,6 +13,30 @@ fn start_repl(){
     let mut user_repl = repl::Repl::new();
     user_repl.run();
 }
+
+fn read_file(tmp: &str) -> String{
+    let filename = Path::new(tmp);
+    let fl = File::open(filename);
+    match fl {
+        Ok(mut f) => {
+            let mut file_content = String::new();
+            match f.read_to_string(&mut file_content) {
+                Ok(_) => file_content,
+                Err(e) => {
+                    println!("{:?}",e);
+                    std::process::exit(1);
+                }
+            }
+            
+        },
+        Err(_) => {
+            println!("Failed to read file");
+            std::process::exit(1);
+
+        }
+    }
+}
+
 
 fn main() {
     start_repl();
