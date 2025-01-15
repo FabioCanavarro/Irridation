41a048ee6e3db78b2b527a83cffeaffb3bda8ae9
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-01-15 21:03:33 +0700

    Updated main function in main.rs to have a cli.yml and let it either start repl or run a program from a specific file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 #[macro_use]
 extern crate nom;
-extern crate clap;
-use std::{fs::File, io::Read, path::Path};
 
+#[macro_use]
+extern crate clap;
+
+use std::{fs::File, io::Read, path::Path};
+use assembler::Assembler;
 use clap::{App,Arg,SubCommand};
+use vm::Vm;
+
 pub mod assembler;
 pub mod instruction;
 pub mod repl;
@@ -39,5 +44,24 @@ fn read_file(tmp: &str) -> String{
 
 
 fn main() {
-    start_repl();
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    let target_file = matches.value_of("INPUT_FILE");
+    match target_file{
+        Some(fl) =>{
+            let program = read_file(fl);
+            let mut vm = Vm::new();
+            let mut asm = Assembler::new();
+            let program = asm.assemble(&program);
+
+            if let Some(p) = program{
+                vm.add_bytes(p);
+                vm.run();
+                std::process::exit(0);
+            }
+ 
+        },
+        None => start_repl(),
+        
+   }
 }
